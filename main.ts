fce221aeea3445b787e9bc99a7d402373db1629f
Sabas <sabasjimenez@gmail.com>
2019-01-01 22:48:09 -0600

    initial commit

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
+namespace gps {
+    let latitude = 0
+    let longitude = 0
+    let results: string[] = []
 
+    /**
+     * Get position latitude and longitude.
+     */
+    //% blockId=gps block="gps get position"
+    //% weight=1
+    export function get_position(): void {
+        latitude = (latitude / 1000000.0);
+        longitude = (longitude / 1000000.0);
+    }
+
+    function encode(): void {
+        let valid_sentence = serial.readLine()
+        let ind = valid_sentence.indexOf(",")
+
+        while (ind != -1) {
+            results.push(valid_sentence.slice(0, ind))
+            valid_sentence = valid_sentence.slice(ind + 1)
+            ind = valid_sentence.indexOf(",")
+        }
+        results.push(valid_sentence)
+        helpers.arrayForEach(results, function (value: string, index: number) {
+            console.log(value)
+        })
+    }
+
+}
